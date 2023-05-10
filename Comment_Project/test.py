5f51f626766c9db2a444821429b0617806848b4e
Niloufar <NilouRahimi2376>
2023-05-10 10:55:41 +0330

    20Ordibehesht

--- Comment_Project/test.py
+++ Comment_Project/test.py
@@ -1,5 +1,5 @@
 from version1 import model
-
+import pandas as pd
 
 
 MAXLEN = 120 #681
@@ -8,6 +8,8 @@ path = '../../saved_model/my_model'
 tokenizer = 'tokenizer.pickle'
 sentence = 'بازی بدی نبود اونقدر هم ارزش نداره ک بخای. بری رفتار پرسنل اصلن خوب نیست بازی باگ نور داره اکتور زعیف واقعن باید تلاشتونو بیشتر کنید'
 
-
+df = pd.read_excel('/Users/niloufar/Desktop/DeepLearning/tf_specialization/comment/spam_or_not1.xlsx')
+sentence = "رفتارشون خوب بود اما معماها بی معنا و الکی بودن، در کل چنتا اتاق تاریک بدون سناریوی جالب. جای لوکیشن هم اصلا مناسب نبود. در کل پیشنهاد نمیکنم."
 output = model(sentence, tokenizer, PADDING, MAXLEN, path)
 print(output)
+# print(sentence)
