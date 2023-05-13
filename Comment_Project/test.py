fe428baceaf336b6406923d13b8ff386e8ffb2b6
Niloufar <NilouRahimi2376>
2023-05-13 16:37:47 +0330

    23Ordibehesht

--- Comment_Project/test.py
+++ Comment_Project/test.py
@@ -1,15 +1,20 @@
 from version1 import model
 import pandas as pd
-
-
+from padded_seq import seq_and_pad
+import pickle
 MAXLEN = 120 #681
 PADDING = 'post'
 path = '../../saved_model/my_model'
 tokenizer = 'tokenizer.pickle'
 sentence = 'بازی بدی نبود اونقدر هم ارزش نداره ک بخای. بری رفتار پرسنل اصلن خوب نیست بازی باگ نور داره اکتور زعیف واقعن باید تلاشتونو بیشتر کنید'
-
-df = pd.read_excel('/Users/niloufar/Desktop/DeepLearning/tf_specialization/comment/spam_or_not1.xlsx')
+sentence = 'این اتاق فرار استاندارد نبود تاچ زیاد میکردن اما تاچ نرمال نه درکل مناسب خانم ها نی داخل بازی از لفظ های قشنگی استفاده نمی'
 sentence = "رفتارشون خوب بود اما معماها بی معنا و الکی بودن، در کل چنتا اتاق تاریک بدون سناریوی جالب. جای لوکیشن هم اصلا مناسب نبود. در کل پیشنهاد نمیکنم."
+# sentence = 'بی ناموساااا'
+sentence = 'افتضاح'
 output = model(sentence, tokenizer, PADDING, MAXLEN, path)
 print(output)
 # print(sentence)
+# with open(tokenizer, 'rb') as handle:
+#     tokenizer = pickle.load(handle)
+
+# print(seq_and_pad(tokenizer, [sentence], PADDING, MAXLEN)[0])
