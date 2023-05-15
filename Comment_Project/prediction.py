4a6fd8ead6de73790eb5cd6cdce83bc1e0e89ac7
Niloufar <NilouRahimi2376>
2023-05-15 13:33:38 +0330

    25Ordibehesht

--- Comment_Project/prediction.py
+++ Comment_Project/prediction.py
@@ -5,5 +5,5 @@ import numpy as np
 def predict(path, padded_sequence):
     model = tf.keras.models.load_model(path)
     output = model.predict(padded_sequence[0:1])
-    output = np.where(output<0.5, 'spam', 'ham')
+    output = np.where(output>0.5, 'spam', 'ham')
     return output[0][0]
